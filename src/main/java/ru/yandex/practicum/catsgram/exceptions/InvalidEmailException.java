9e7e5ccc494b7b2d8b7ec41bc253fa24e859704a
PracticumJava <ragozina.anastasiia@yandex.ru>
2022-04-06 00:38:57 +0300

    add logging_logging-your-messages

--- src/main/java/ru/yandex/practicum/catsgram/exceptions/InvalidEmailException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.catsgram.exceptions;
-
-public class InvalidEmailException extends RuntimeException {
-    public InvalidEmailException(String s) {
-        super(s);
-    }
-}
