d74338c8d0e98120dc0bae3c2e83a9e36d68493e
kpmy <petryxa.clever@gmail.com>
2022-04-20 14:50:04 +0300

    тема Параметры запроса и формат ответа, урок 3

--- src/main/java/ru/yandex/practicum/catsgram/controller/PostFeedController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostFeedController.java
@@ -5,41 +5,74 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import ru.yandex.practicum.catsgram.model.FriendsParams;
 import ru.yandex.practicum.catsgram.model.Post;
 import ru.yandex.practicum.catsgram.service.PostService;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
-@RestController
+@RestController()
 public class PostFeedController {
-    PostService postService;
 
-    public PostFeedController(PostService postService) {
+    private final PostService postService;
+
+    public PostFeedController(PostService postService){
         this.postService = postService;
     }
 
     @PostMapping("/feed/friends")
-
-    public List<Post> getFriends(@RequestBody String listFriend) {
-        List<Post> resultPost = new ArrayList<>();
-        System.out.println("listFriend = " + listFriend);
+    List<Post> getFriendsFeed(@RequestBody String params){
+        ObjectMapper objectMapper = new ObjectMapper();
+        FriendsParams friendsParams;
         try {
-            var jsonString = new ObjectMapper().readValue(listFriend, String.class);
-            var friendsParams = new ObjectMapper().readValue(jsonString, FriendsParams.class);
-            if (friendsParams == null) {
-                throw new IllegalArgumentException("Не верный запрос");
-            }
-            for (String friend : friendsParams.getFriends()) {
-                resultPost.addAll(postService.findAllByUserEmail(friend
-                        , friendsParams.getSize()
-                        , friendsParams.getSort()));
-            }
-        } catch (JsonProcessingException err) {
-            throw new IllegalArgumentException("Неверный запрос");
+            String paramsFromString = objectMapper.readValue(params, String.class);
+            friendsParams = objectMapper.readValue(paramsFromString, FriendsParams.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("невалидный формат json", e);
         }
 
-        return resultPost;
+        if(friendsParams != null){
+            List<Post> result = new ArrayList<>();
+            for (String friend : friendsParams.friends) {
+                result.addAll(postService.findAllByUserEmail(friend, friendsParams.size, friendsParams.sort));
+            }
+            return result;
+        } else {
+            throw new RuntimeException("неверно заполнены параметры");
+        }
+    }
+
+    static class FriendsParams {
+        private String sort;
+
+        private Integer size;
+
+        private List<String> friends;
+
+        public String getSort() {
+            return sort;
+        }
+
+        public void setSort(String sort) {
+            this.sort = sort;
+        }
+
+        public Integer getSize() {
+            return size;
+        }
+
+        public void setSize(Integer size) {
+            this.size = size;
+        }
+
+        public List<String> getFriends() {
+            return friends;
+        }
+
+        public void setFriends(List<String> friends) {
+            this.friends = friends;
+        }
     }
 }
