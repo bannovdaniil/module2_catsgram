bc30f678ed4f24faf5d273bc3fce6db7fb7ed0d1
bannovdaniil <baddan@mail.ru>
2022-05-27 17:26:32 +0300

    add: User class

---
+++ src/main/java/ru/yandex/practicum/catsgram/model/User.java
@@ -0,0 +1,24 @@
+package ru.yandex.practicum.catsgram.model;
+
+import java.time.LocalDate;
+
+public class User {
+    private String email;
+    private String nickname;
+    private LocalDate birthdate;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return email != null ? email.equals(user.email) : user.email == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return email != null ? email.hashCode() : 0;
+    }
+}
