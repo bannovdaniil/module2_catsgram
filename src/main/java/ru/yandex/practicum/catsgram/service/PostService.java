28741a84dcdfad1547f792027011a373a27426e4
bannovdaniil <baddan@mail.ru>
2022-06-09 22:16:32 +0300

    6. Развиваем Котограм - UserService

--- src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
+++ src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
@@ -1,7 +1,10 @@
 package ru.yandex.practicum.catsgram.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.catsgram.exception.UserNotFoundException;
 import ru.yandex.practicum.catsgram.model.Post;
+import ru.yandex.practicum.catsgram.model.User;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,12 +12,23 @@ import java.util.List;
 @Service
 public class PostService {
     private final List<Post> posts = new ArrayList<>();
+    private final UserService userService;
+
+    @Autowired
+    public PostService(UserService userService) {
+        this.userService = userService;
+    }
 
     public List<Post> findAll() {
         return posts;
     }
 
     public Post create(Post post) {
+        String email = post.getAuthor();
+        User user = userService.findUserByEmail(email);
+        if (user == null) {
+            throw new UserNotFoundException("Пользователь <" + email + "> не найден");
+        }
         posts.add(post);
         return post;
     }
